91c4bf4af28af2e2d41d3fdfb9578c77ead10d55
Garam <garam@nhnnext.org>
2014-04-16 19:02:12 +0900

    second commit

---
+++ w6_lab2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            PrintBanner();
+            PrintDetails();
+            Console.ReadLine();
+        }
+
+        public static void PrintBanner()
+        {
+            Console.WriteLine("**********************************");
+            Console.WriteLine("******** Lottery Numbers *********");
+            Console.WriteLine("**********************************");
+        }
+
+        public static void PrintDetails()
+        {
+            int[] numbers = new int[6] { 0, 0, 0, 0, 0, 0};
+            
+            PutIndex(ref numbers);
+            PrintResult(ref numbers);
+        }
+        public static string GetName()
+        {
+            Console.Write("Enter yout name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        public static int[] PutIndex(ref int[] numbers)
+        {
+            Random random = new Random(); 
+            int index = 0;
+            int randomNumber;
+            int loop;
+            
+            do
+            {
+                randomNumber = random.Next(1, 46);
+                for (loop = 0; loop < index; loop++)
+                {
+                    if (numbers[loop] == randomNumber)
+                    {
+                        break;
+                    }
+
+                }
+                if (index==loop && numbers[index] != randomNumber)
+                {
+                    numbers[index] = randomNumber;
+                    index++;
+                }
+
+            } while (index < numbers.Length);
+
+
+            return numbers;
+        }
+        public static void PrintResult(ref int[] numbers)
+        {
+            Console.Write("Name : {0}, Numbers : [", GetName());
+
+            foreach (int number in numbers)
+            {
+                Console.Write("{0} ", number);
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ w6_lab3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace w6_lab3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine (Palindrome (Input()));
+		}
+	
+		public static string Input ()
+		{
+			Console.Write ("Input word : ");
+			string input = Console.ReadLine ();
+			return input;
+		}
+		public static string Palindrome (string input)
+		{
+			string palindrome = "T";
+			while (palindrome == "T" && input.Length > 2){
+				if (input [0] == input [input.Length - 1]) {
+					input = input.Substring (1, input.Length - 2);
+					Palindrome (input);
+				} else {
+					palindrome = "F";
+				}
+			}
+			return palindrome;
+		} 
+
+		/*
+		public static void Palindrome ()
+		{
+			Console.Write ("Input word like 'm i l k', 'm o m' : ");
+			String[] input = Console.ReadLine ().Split (' ');
+			string palindrome = "T";
+			for (int i = 0; i < (input.Length / 2) && palindrome == "T"; i++) {
+				if (input [i] != input [input.Length - i - 1]) {
+					palindrome = "F";
+				}
+			}
+			Console.WriteLine (palindrome);
+		}*/
+	}
+}
